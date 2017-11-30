8dfcbe2ac6ebc36a1095367c3b16471138a2e1f1
anamali <caoimhe2>
2017-11-30 21:09:54 +0000

    Commit Changes

--- app/src/main/java/com/example/ovidiu/easyworkers/MainActivity.java
+++ app/src/main/java/com/example/ovidiu/easyworkers/MainActivity.java
@@ -43,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_aboutus) {
+
         }
 
         return super.onOptionsItemSelected(item);
